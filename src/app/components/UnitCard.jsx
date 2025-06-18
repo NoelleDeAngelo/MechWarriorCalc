8573b13c484c8fc754763f0328b4c9e033150d78
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2025-06-17 19:03:43 -0700

    Add space for special rules and images of units

--- src/app/components/UnitCard.jsx
+++ src/app/components/UnitCard.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRoster } from "@/context/RosterContext";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "./unitCard.module.css";
 
 
@@ -8,6 +8,7 @@ import styles from "./unitCard.module.css";
 export default function UnitCard({ unit, expandUnit, index }) {
   const { roster, setRoster } = useRoster();
   const [damage, setDamage] = useState(unit.damage || 0);
+  const [currentHeat, setCurrentHeat] = useState(unit.currentHeat || 0);
   const [alive, setAlive] = useState(unit.alive);
 
 
@@ -20,6 +21,27 @@ export default function UnitCard({ unit, expandUnit, index }) {
       roster[index].alive = false;
     }
   }
+  const repairUnit = () => {
+    if (damage > 0) {
+      setDamage((prev) => prev - 1);
+      roster[index].damage = damage - 1;
+    }
+  }
+
+  const takeHeat = () => {
+    if (currentHeat < unit.heat.mechSpeed.length - 1) {
+      setCurrentHeat((prev) => prev + 1);
+      roster[index].currentHeat = currentHeat + 1;
+    }
+  };
+
+  const vent = () => {
+    let ventNum = unit.vent;
+    if (currentHeat > 0) {
+      setCurrentHeat((prev) => prev - ventNum);
+      roster[index].currentHeat = currentHeat - ventNum;
+    }
+  };
 
   const checkBackground = (bgCode) => {
     if (!bgCode) return;
@@ -47,47 +69,159 @@ export default function UnitCard({ unit, expandUnit, index }) {
 
   return (
     <div>
+
+      <img
+        className={styles.unitImage}
+        src={`/assets/units/${unit.wId}.jpg`}
+        alt={unit.name}
+      />
       <h2>{unit.name}</h2>
-      <span className={styles.close} onClick={expandUnit(index)} >X</span>
+      <span className={styles.close} onClick={expandUnit(index)}>
+        X
+      </span>
       <a
         target="blank"
         href={`https://www.warrenborn.com/Unit.php?ID=${unit.wId}`}
       >
         See on Warrenborn
       </a>
-      <section>
+      <section className={styles.damageTable}>
         {unit.primary && (
-          <p>
+          <p className={styles.stat}>
             Primary: {unit.primary[0]} @ {unit.primary[1]}
           </p>
         )}
         {unit.secondary && (
-          <p>
+          <p className={styles.stat}>
             Secondary: {unit.secondary[0]} @ {unit.secondary[1]}
           </p>
         )}
         {unit.capacity && <p>Capacity: {unit.capacity}</p>}
         {/* { unit.artillery && <p>Artillery: {unit.artillery}</p> } */}
         {unit.artilleryRange && <p>Artillery Range: {unit.artilleryRange}</p>}
+        {unit.vent && <p className={styles.stat}>Vent: {unit.vent}</p>}
       </section>
       <section>
-        <button
-          onClick={() => {takeDamage()}}> take damage</button>
-        {Object.entries(unit.table1).map(([type, value]) => {
-          if (type === "repair") return null; // Skip repair
-          return (
-            <div className={styles.statsContainer} key={type}>
-              <img src={`/assets/icons/${type}.jpg`}></img>
-              {alive ? (
-                <span className={`${styles.tableStat} ${checkBackground(value[damage][1])}`}>{value[damage][0]}</span>
-              ) : (
-                <span className={styles.dead}>
-                  <img src={`/assets/icons/bullets.jpg`}></img>
-                </span>
-              )}
+        <div>
+          <button
+            className={styles.button}
+            onClick={() => {
+              takeDamage();
+            }}
+          >
+            {" "}
+            Take Damage
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => {
+              repairUnit();
+            }}
+          >
+            {" "}
+            Repair
+          </button>
+        </div>
+        <div className={styles.damageContainer}>
+          {Object.entries(unit.table1).map(([type, value]) => {
+            if (type === "repair") return null; // Skip repair
+            return (
+              <div className={styles.statsContainer} key={type}>
+                <img width="35px" src={`/assets/icons/${type}.jpg`}></img>
+                {alive ? (
+                  <span
+                    className={`${styles.tableStat} ${checkBackground(
+                      value[damage][1]
+                    )}`}
+                  >
+                    {value[damage][0]}
+                  </span>
+                ) : (
+                  <span className={styles.dead}>
+                    <img src={`/assets/icons/bullets.jpg`}></img>
+                  </span>
+                )}
+              </div>
+            );
+          })}
+          <div className={styles.rulesContainer}>
+            <details className={styles.rule}>
+              <summary className={styles.ruleSummary}>
+                <span className={styles.green}></span>INFILTRATE (optional)
+              </summary>
+              <p>
+                When preparing the battlefield, deploy this unit after all
+                figures without Infiltrate have been deployed. Deploy figures
+                with Infiltrate starting with the first player and proceeding
+                clockwise. Vehicles and 'Mechs with Infiltrate may be deployed
+                up to their speed values away from their controllers' deployment
+                zones; infantry, up to twice their speed values away. No unit
+                may be deployed in an opposing player's deployment zone.
+              </p>
+            </details>
+            <details className={styles.rule}>
+              <summary className={styles.ruleSummary}>
+                <span className={styles.blue}></span>
+                COMMAND
+              </summary>
+              <p>
+                When preparing the battlefield, deploy this unit after all
+                figures without Infiltrate have been deployed. Deploy figures
+                with Infiltrate starting with the first player and proceeding
+                clockwise. Vehicles and 'Mechs with Infiltrate may be deployed
+                up to their speed values away from their controllers' deployment
+                zones; infantry, up to twice their speed values away. No unit
+                may be deployed in an opposing player's deployment zone.
+              </p>
+            </details>
+          </div>
+        </div>
+        {unit.vent && (
+          <div className={styles.heatContainer}>
+            <div>
+              <button
+                className={styles.button}
+                onClick={() => {
+                  takeHeat();
+                }}
+              >
+                {" "}
+                Take Heat
+              </button>
+              <button
+                className={styles.button}
+                onClick={() => {
+                  vent();
+                }}
+              >
+                {" "}
+                Vent
+              </button>
             </div>
-          );
-        })}
+            <div className={styles.damageContainer}>
+              {Object.entries(unit.heat).map(([type, value]) => {
+                return (
+                  <div className={styles.statsContainer} key={type}>
+                    <img width="35px" src={`/assets/icons/${type}.jpg`}></img>
+                    {alive ? (
+                      <span
+                        className={`${styles.tableStat} ${checkBackground(
+                          value[currentHeat][1]
+                        )}`}
+                      >
+                        {value[currentHeat][0]}
+                      </span>
+                    ) : (
+                      <span className={styles.dead}>
+                        <img src={`/assets/icons/bullets.jpg`}></img>
+                      </span>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </section>
     </div>
   );
--- src/data/units.js
+++ src/data/units.js
@@ -1,72 +1,166 @@
-
 //This is the main object that contains all the units.
 //The units are identified by their unique id.
 
-
-
 const units = {
-
   //------Mechs-----
 
   mechs: {
     AOD104: {
       alive: true, //This is used to determine if the unit is still alive in the game.
       name: "Hatchetman",
-      wId: "AOD104", //This is the unique id for the unit. It should be unique across all units.
+      wId: "AOD104", //This is the unique id for the unit. It should be unique across all units. wId instead of ID
       variant: "HCT2F-M",
       points: 133,
-      class: "Medium",//this is important for equip and pilot upgrades
+      class: "Medium",
       faction: "Bannson's Raiders",
-      factionIcon: "iconBR", //For list-building. Do we need this if we have "faction" above?
       primary: [1, "0/0"],
       secondary: [1, "0/12"],
-      capacity: null,
-      artillery: null,
-      artilleryRange: null,
       //Only Mechs can take equipment and pilot upgrades. They can take /up to/ either 2 equipment or 1 equipment and 1 pilot...
       vent: 2, //Only Mechs have Vent
       table1: {
-        meleeDamage: [[3, "greenS"], [3, "greenS"], [2, "greenS"], [2, "greenS"], [2, "greenS"], [2, null], [2, null], [1, null], [1, null], [1, null], [0, null], [0, null]],
-        ballisticDamage: [[3, null], [3, null], [3, null], [2, null], [2, null], [2, null], [2, null], [2, null], [0, null], [0, null], [0, null], [0, null],],
-        mechSpeed: [[8, "blueS"], [7, "blueS"], [7, "blueS"], [6, "blueS"], [6, null], [6, null], [6, null], [6, null], [5, null], [5, null], [5, null], [4, null], ],
-        attack: [[9, null], [9, null], [8, null], [8, null], [8, null], [7, null], [7, null], [6, null], [6, null], [6, null], [5, null], [4, null], ],
-        defense: [[20, null], [20, null], [20, null], [19, null], [19, null], [19, null], [18, null], [18, null], [17, null], [17, null], [16, null], [15, null], ],
+        meleeDamage: [
+          [3, "greenS"],
+          [3, "greenS"],
+          [2, "greenS"],
+          [2, "greenS"],
+          [2, "greenS"],
+          [2, null],
+          [2, null],
+          [1, null],
+          [1, null],
+          [1, null],
+          [0, null],
+          [0, null],
+        ],
+        ballisticDamage: [
+          [3, null],
+          [3, null],
+          [3, null],
+          [2, null],
+          [2, null],
+          [2, null],
+          [2, null],
+          [2, null],
+          [0, null],
+          [0, null],
+          [0, null],
+          [0, null],
+        ],
+        mechSpeed: [
+          [8, "blueS"],
+          [7, "blueS"],
+          [7, "blueS"],
+          [6, "blueS"],
+          [6, null],
+          [6, null],
+          [6, null],
+          [6, null],
+          [5, null],
+          [5, null],
+          [5, null],
+          [4, null],
+        ],
+        attack: [
+          [9, null],
+          [9, null],
+          [8, null],
+          [8, null],
+          [8, null],
+          [7, null],
+          [7, null],
+          [6, null],
+          [6, null],
+          [6, null],
+          [5, null],
+          [4, null],
+        ],
+        defense: [
+          [20, null],
+          [20, null],
+          [20, null],
+          [19, null],
+          [19, null],
+          [19, null],
+          [18, null],
+          [18, null],
+          [17, null],
+          [17, null],
+          [16, null],
+          [15, null],
+        ],
         repair: [["green"]],
       },
-      heat: { //We didn't discuss how to represent positive and negative numbers...
-        meleeDamage: [[0, "greenS"], [0, null], [+1, null], [+1, null], [+1, null], [null, "Shutdown"]],
-        ballisticDamage: [[0, "greenS"], [0, null], [0, null], [0, null], [-1, "yellowS"], [null, "Shutdown"]],
-        mechSpeed: [[0, "greenS"], [0, null], [+1, null], [+2, "yellowS"], [+2, "yellowS"], [null, "Shutdown"]],
+      heat: {
+        meleeDamage: [
+          [0, "greenS"],
+          [0, null],
+          [1, null],
+          [1, null],
+          [1, null],
+        ],
+        ballisticDamage: [
+          [0, "greenS"],
+          [0, null],
+          [0, null],
+          [0, null],
+          [-1, "yellowS"],
+        ],
+        mechSpeed: [
+          [0, "greenS"],
+          [0, null],
+          [1, null],
+          [2, "yellowS"],
+          [2, "yellowS"],
+        ],
       },
     },
   },
 
+  //-----Vehicles and Infantry-----
 
-
-
-    //-----Vehicles and Infantry-----
-
-    vehicles: {
-      AOD071: {
-        alive: true,
-        name: "SM2 Heavy Artillery Vehicle",
-        wId: "AOD071",
-        points: 36,
-        faction: "Bannson's Raiders",
-        factionIcon: "iconBR",
-        primary: [1, "0/10"],
-        artilleryRange: 22,
-        capacity: null,
-        artillery: "AOD071.jpg",
-        table1: {
-          ballisticDamage: [[2, "redS"], [2, "redS"], [2, "redS"], [2, "redC"], [0, null], ],
-          trackedSpeed: [[8, null], [8, null], [7, null], [7, null], [6, null], ],
-          attack: [[7, null], [7, null], [6, null], [5, null], [4, "blackS"], ],
-          defense: [[20, "greyS"], [19, "greyS"], [18, null], [18, null], [17, null], ],
-          repair: [["green"]],
-        },
+  vehicles: {
+    AOD071: {
+      alive: true,
+      name: "SM2 Heavy Artillery Vehicle",
+      wId: "AOD071",
+      points: 36,
+      faction: "Bannson's Raiders",
+      primary: [1, "0/10"],
+      artilleryRange: 22,
+      capacity: null,
+      table1: {
+        ballisticDamage: [
+          [2, "redS"],
+          [2, "redS"],
+          [2, "redS"],
+          [2, "redC"],
+          [0, null],
+        ],
+        trackedSpeed: [
+          [8, null],
+          [8, null],
+          [7, null],
+          [7, null],
+          [6, null],
+        ],
+        attack: [
+          [7, null],
+          [7, null],
+          [6, null],
+          [5, null],
+          [4, "blackS"],
+        ],
+        defense: [
+          [20, "greyS"],
+          [19, "greyS"],
+          [18, null],
+          [18, null],
+          [17, null],
+        ],
+        repair: [["green"]],
       },
     },
+  },
 };
 
 export default units;
